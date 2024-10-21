14b7e4552c502be9c7c3b045e0fd3e3c8ecb99d7
belinda267 <104494348+belinda267@users.noreply.github.com>
2024-10-21 16:11:25 +1030

    Update homeProductionOp.php

--- homeProductionOp.php
+++ homeProductionOp.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Belinda Hok"/>
     <title>Production Operator Home</title>
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="Styles/styles.css">
 </head>
 
 <body>
